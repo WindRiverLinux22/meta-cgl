0045ab112006746a5e89927b682c922176c7f448
Mingli Yu <mingli.yu@windriver.com>
2020-08-07 16:47:15 +0800

    ucarp: don't hardcode the path for ifconfig
    Don't hardcode the path for the ifconfig command in case the path
    for ifconfig on the system may be different from the hardcoded.
    
    Signed-off-by: Mingli Yu <mingli.yu@windriver.com>
    Signed-off-by: Jeremy A. Puhlman <jpuhlman@mvista.com>

--- meta-cgl-common/recipes-cgl/ucarp/ucarp/vip-down.sh
+++ meta-cgl-common/recipes-cgl/ucarp/ucarp/vip-down.sh
@@ -4,4 +4,4 @@ exec 2> /dev/null
 #/sbin/ip addr del "$2"/24 dev "$1"
 
 # or alternatively:
-/sbin/ifconfig "$1":254 down
+ifconfig "$1":254 down
--- meta-cgl-common/recipes-cgl/ucarp/ucarp/vip-up.sh
+++ meta-cgl-common/recipes-cgl/ucarp/ucarp/vip-up.sh
@@ -4,4 +4,4 @@ exec 2> /dev/null
 #/sbin/ip addr add "$2"/24 dev "$1"
 
 # or alternatively:
-/sbin/ifconfig "$1":254 "$2" netmask 255.255.255.0 
+ifconfig "$1":254 "$2" netmask 255.255.255.0
